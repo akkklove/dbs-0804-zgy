9efd12c081f2b397291f6ad2948d6640652f8510
akkklove <49298244+akkklove@users.noreply.github.com>
2022-08-04 10:28:33 +0800

    Add files via upload

---
+++ templates/app.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+from flask import Flask, request, render_template
+
+
+# In[ ]:
+
+
+import joblib
+
+
+# In[ ]:
+
+
+app = Flask(__name__)
+
+
+# In[ ]:
+
+
+@app.route("/", methods = ["GET", "POST"])
+def index():
+    if request.method == "POST":
+        return(render_template("index.html",result1 = "OK", result2 = "OK"))
+    else:
+        return(render_template("index.html",result1 = "WAITING",result2 = "WATTING"))
+
+
+# In[ ]:
+
+
+if __name__ == "__main__":
+    app.run()
+
+
+# In[ ]:
+
+
+
+
